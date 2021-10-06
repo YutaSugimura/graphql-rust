df2c892d88e8b1bbdb19967487e5956fb64e6d96
YutaSugimura <nextc3m@gmail.com>
2021-10-06 22:51:21 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 use std::sync::Arc;
 
 use actix_cors::Cors;
-use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer, http::header};
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
@@ -48,11 +48,16 @@ async fn main() -> io::Result<()> {
             .data(schema.clone())
             .wrap(middleware::Logger::default())
             .wrap(
-                Cors::new()
-                    .allowed_methods(vec!["POST", "GET"])
+                Cors::default()
+                    .allowed_origin("http://app.com:8080")
+                    .allowed_origin_fn(|origin, _req_head| {
+                        origin.as_bytes().starts_with(b"http://localhost")
+                    })
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
                     .supports_credentials()
-                    .max_age(3600)
-                    .finish(),
+                    .max_age(3600),
             )
             .service(web::resource("/graphql").route(web::post().to(graphql)))
             .service(web::resource("/graphiql").route(web::get().to(graphiql)))
